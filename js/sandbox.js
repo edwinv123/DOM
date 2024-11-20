247a4a5120be064813955a9e50e528597070cf82
edwinv123 <126evalentinnunez@frhsd.com>
2024-11-20 12:50:30 -0500

    first commit

---
+++ js/sandbox.js
@@ -0,0 +1,9 @@
+// var titles = document.getElementsByClassName('title');
+
+// // console.log(Array.isArray(titles));
+// // console.log(Array.isArray(Array.from(titles)));
+
+// Array.from(titles).forEach(function(items)){
+//     console.log(items);
+// } (Video 3)
+
